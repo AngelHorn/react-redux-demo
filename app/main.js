33d5be69d38a7fc092fb7c743502d16717629e8d
AngelHorn <58268636@qq.com>
2015-09-18 23:23:37 +0800

    create project

---
+++ app/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Component extends React.Component {
+    render() {
+        return <h1>Hello world!!!</h1>;
+    }
+}
+
+React.render(< Component />, document.body);
